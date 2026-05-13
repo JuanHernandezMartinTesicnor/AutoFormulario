5ab6c7de2ec71d6f97603bbd1a7271f62c5d8566
JuanHernandezMartinTesicnor <jhmartin@tesicnor.com>
2026-05-13 14:24:43 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const formRoutes = require("./routes/formRoutes");
 
 const app = express();
@@ -7,7 +9,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+/* =========================
+   API
+========================= */
+
 app.use("/api", formRoutes);
+
+/* =========================
+   FRONTEND
+========================= */
+
+// Servir archivos frontend
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+// Ruta principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
+/* =========================
+   START SERVER
+========================= */
 
 const PORT = process.env.PORT || 3000;
 
