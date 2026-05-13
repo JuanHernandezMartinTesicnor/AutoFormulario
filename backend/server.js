0f88bd9290a3cfe446afd8fcbf158623dc0b197b
JuanHernandezMartinTesicnor <jhmartin@tesicnor.com>
2026-05-13 14:22:01 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ app.use(express.json());
 
 app.use("/api", formRoutes);
 
-app.listen(3000, () => {
-  console.log("Servidor en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
