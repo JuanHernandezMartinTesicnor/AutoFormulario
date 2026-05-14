1de79e02941be0d3a3bd6b8b1252da06c78411f9
JuanHernandezMartinTesicnor <jhmartin@tesicnor.com>
2026-05-14 12:09:47 +0200

    Update pdfService.js

--- backend/services/pdfService.js
+++ backend/services/pdfService.js
@@ -2,9 +2,16 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
 
+const isLinux = process.platform === "linux";
+
 async function generatePDF(data) {
   const browser = await puppeteer.launch({
     headless: true,
+
+    ...(isLinux && {
+      executablePath: "/snap/bin/chromium"
+    }),
+
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
