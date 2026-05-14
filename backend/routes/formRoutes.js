a29373f63a4bbc0399709e77580f29c4b1055b63
JuanHernandezMartinTesicnor <jhmartin@tesicnor.com>
2026-05-14 16:05:32 +0200

    Update formRoutes.js

--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const router = express.Router();
+
 const { generatePDF } = require("../services/pdfService");
 
 router.post("/generate-pdf", async (req, res) => {
   try {
+    console.log("PETICION PDF RECIBIDA");
+
     const pdf = await generatePDF(req.body);
+
+    console.log("PDF GENERADO");
 
     res.set({
       "Content-Type": "application/pdf",
-      "Content-Disposition": "attachment; filename=formulario.pdf"
+      "Content-Length": pdf.length
     });
 
     res.send(pdf);
+
   } catch (err) {
+    console.error("ERROR GENERANDO PDF:");
     console.error(err);
-    res.status(500).send("Error");
+
+    res.status(500).send(err.toString());
   }
 });
 
