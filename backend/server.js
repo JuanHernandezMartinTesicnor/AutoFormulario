263b36963b72276cbb65bdbf450cbba0439dd501
JuanHernandezMartinTesicnor <jhmartin@tesicnor.com>
2026-07-31 10:49:51 +0200

    more photos

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,12 @@ const app = express();
 app.use(cors());
 
 app.use(express.json({
-    limit: "50mb"
+    limit: "300mb"
 }));
 
 app.use(express.urlencoded({
     extended: true,
-    limit: "50mb"
+    limit: "300mb"
 }));
 
 /* =========================
